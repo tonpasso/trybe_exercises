5aede43008d0684ccb156531e3b1b3df8dae9859
tonpasso <wellpasso@gmail.com>
2021-02-28 18:26:39 -0300

    Ex_01 - Operações aritméticas básicas

---
+++ Fundamentos_Desenvolvimento_Web/Bloco_04_Introducao-a-Javascript-e-Logica-programacao/Dia_01/exercicio_01.js
@@ -0,0 +1,14 @@
+let a = 12;
+let b = 5;
+
+let sum = a + b;
+
+let sub = a - b;
+
+let mult = a * b;
+
+let div = a / b;
+
+let mod = a % b;
+
+console.log(`Soma: ${sum}, Subtração: ${sub}, Multiplicação: ${mult}, Divisão: ${div}, Módulo: ${mod}`);
