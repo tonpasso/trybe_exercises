1619129dbb6db195a9b15e9aa61c0fdf61a0cadc
tonpasso <wellpasso@gmail.com>
2021-03-01 11:42:16 -0300

    exercicio_04

---
+++ Fundamentos_Desenvolvimento_Web/Bloco_04_Introducao-a-Javascript-e-Logica-programacao/Dia_02/exercicio_04.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  soma += numbers[i];   
+}
+
+let mediaAritmetica = soma / numbers.length; 
+
+if (mediaAritmetica > 20) {
+  console.log('Valor maior que 20');
+} else {
+  console.log('Valor menor ou igua a 20');
+}
