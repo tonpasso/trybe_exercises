b43d12f9ece89b234b178a5a1e7e66eeeca75eb3
tonpasso <wellpasso@gmail.com>
2021-03-24 09:43:43 -0300

    modificando para template literals

--- Fundamentos_Desenvolvimento_Web/Bloco_07_Introdução-Javascript-ES6-Testes-Unitarios/Dia_01/exercises/introducao-a-javascript-es6-e-testes-unitarios_1/parteI_ex_01.js
+++ Fundamentos_Desenvolvimento_Web/Bloco_07_Introdução-Javascript-ES6-Testes-Unitarios/Dia_01/exercises/introducao-a-javascript-es6-e-testes-unitarios_1/parteI_ex_01.js
@@ -1,3 +1,21 @@
+// modificando para arrow function
+
+// const testingScope = escopo => {
+//   if (escopo === true) {
+//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//     console.log(ifScope);
+//   } else {
+//     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+//   console.log(ifScope + ' o que estou fazendo aqui ? :O'); 
+// }
+
+// testingScope(true);
+
+// template literals
+
 const testingScope = escopo => {
   if (escopo === true) {
     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -7,7 +25,7 @@ const testingScope = escopo => {
     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
     console.log(elseScope);
   }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O'); 
+  console.log(`${ifScope} o que estou fazendo aqui ? :0`);  
 }
 
 testingScope(true);
