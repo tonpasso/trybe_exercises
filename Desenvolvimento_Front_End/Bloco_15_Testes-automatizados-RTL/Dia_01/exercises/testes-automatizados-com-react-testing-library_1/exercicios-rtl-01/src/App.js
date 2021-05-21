4c7f0c98981f08292f2924199508df5a399a163b
tonpasso <wellpasso@gmail.com>
2021-05-21 11:18:00 -0300

    iniciando

---
+++ Desenvolvimento_Front_End/Bloco_15_Testes-automatizados-RTL/Dia_01/exercises/testes-automatizados-com-react-testing-library_1/exercicios-rtl-01/src/App.js
@@ -0,0 +1,16 @@
+    // App.js
+    import React from 'react';
+    import './App.css';
+    
+    function App() {
+      return (
+        <div className="App">
+          <label htmlFor="id-email">
+            Email
+          </label>
+          <input id="id-email" type="email" />
+        </div>
+      );
+    }
+    
+    export default App;
