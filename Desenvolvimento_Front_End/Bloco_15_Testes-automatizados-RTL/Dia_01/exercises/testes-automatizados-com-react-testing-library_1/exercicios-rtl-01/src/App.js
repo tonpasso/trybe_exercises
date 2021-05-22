6710ed6bdfd7493ff3cfd176ae9ec519dc88dc89
tonpasso <wellpasso@gmail.com>
2021-05-22 15:34:22 -0300

    exercicio 1

--- Desenvolvimento_Front_End/Bloco_15_Testes-automatizados-RTL/Dia_01/exercises/testes-automatizados-com-react-testing-library_1/exercicios-rtl-01/src/App.js
+++ Desenvolvimento_Front_End/Bloco_15_Testes-automatizados-RTL/Dia_01/exercises/testes-automatizados-com-react-testing-library_1/exercicios-rtl-01/src/App.js
@@ -1,16 +1,55 @@
-    // App.js
-    import React from 'react';
-    import './App.css';
-    
-    function App() {
-      return (
-        <div className="App">
-          <label htmlFor="id-email">
-            Email
-          </label>
-          <input id="id-email" type="email" />
-        </div>
-      );
-    }
-    
-    export default App;
+// App.js
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      saveEmail: "",
+    };
+  }
+
+  changeEmail(value) {
+    this.setState({
+      email: value,
+    })
+  }
+
+  changeSaveEmail(value) {
+    this.setState({ 
+      saveEmail: value,
+      email: '',
+    })
+  }
+
+  render() {
+    const { email, saveEmail } = this.state;
+    return (
+      <div className="App">
+        <label htmlFor="id-email">
+          Email
+          <input 
+            id="id-email" 
+            type="email" 
+            value={ email }
+            onChange={ (event) => this.changeEmail(event.target.value)}
+           />
+        </label>
+        <input
+            id="btn-enviar"
+            type="button"
+            data-testid="id-send"
+            value="Enviar"
+            onClick={ () => this.changeSaveEmail(email) }
+        />
+        
+        <input id="btn-id" type="button" value="Voltar"/>
+        <h3 data-testid="id-email-user">{`Valor: ${ saveEmail }`}</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
--- Desenvolvimento_Front_End/Bloco_15_Testes-automatizados-RTL/Dia_01/exercises/testes-automatizados-com-react-testing-library_1/exercicios-rtl-01/src/App.test.js
+++ Desenvolvimento_Front_End/Bloco_15_Testes-automatizados-RTL/Dia_01/exercises/testes-automatizados-com-react-testing-library_1/exercicios-rtl-01/src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Verifica se há um campo input de email na tela', () => {
+  // Acessar os elementos da tela
+  const { getByLabelText } = render(<App />);
+  const inputEmail = getByLabelText('Email');
+
+  // Fazer o teste
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail.type).toBe('email'); 
 });
+
+test('Verifica se há dois botões', () => {
+  // Acessar os elementos da tela
+  const { getAllByRole } = render(<App />);
+  const button = getAllByRole('button');
+
+  // Fazer o teste
+  expect(button.length).toBe(2);
+});
+
+test('Verifica se há um botão \"Enviar\"', () => {
+  // Acessar os elementos da tela
+  const { getByTestId } = render(<App />);
+  const button = getByTestId('id-send');
+
+  // Fazer o teste
+  expect(button).toBeInTheDocument();
+  expect(button).toHaveValue('Enviar');
+})
+
+// Acessar os elementos da tela
+
+// Interagir com eles (se houver necessidade)
+
+// Fazer o teste
